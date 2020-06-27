aa3d0a10122fb1dc97a911eaf4dd11684dacc73a
Mark Young <markyaaronyoung@gmail.com>
2020-06-27 12:54:39 -0500

    created a working logout event that logs user out inside a navbar component

--- src/javascripts/components/auth/auth.js
+++ src/javascripts/components/auth/auth.js
@@ -1,7 +1,11 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import utils from '../../helpers/utils';
 
 const signMeIn = () => {
-  console.warn('yeah I\'ll sign you in');
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
 };
 
 const loginButton = () => {
