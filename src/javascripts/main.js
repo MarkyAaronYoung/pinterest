470c0ca8e37851ccd13268b2f9e6b86a12ff48f5
Mark Young <markyaaronyoung@gmail.com>
2020-06-27 11:36:32 -0500

    login button prints to dom and event fires

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,14 @@
+import auth from './components/auth/auth';
+
 import '../styles/main.scss';
 
-console.error('hi');
+// be able to login to our app
+// see a login button if we are not logged in they should see an h1 that says pinterest
+// when logged in they should see a navbar with a brand, logout button, and an h1 on the page that says board
+
+const init = () => {
+  console.warn('hi');
+  auth.loginButton();
+};
+
+init();
